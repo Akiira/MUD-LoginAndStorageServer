a69e373c20ddce38e37cdaa16a72b383464e35b2
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-02 00:33:40 -0400

    updated ServerMessage

--- ServerMessage.go
+++ ServerMessage.go
@@ -1,6 +1,10 @@
 // ServerMessage
 package main
 
+import (
+	"strings"
+)
+
 const (
 	REDIRECT = 1
 )
@@ -12,4 +16,19 @@ type ServerMessage struct {
 
 func newServerMessage(typeOfMsg int, msg string) ServerMessage {
 	return ServerMessage{MsgType: typeOfMsg, Value: newFormattedString(msg)}
+}
+
+func (msg *ServerMessage) getMessage() string {
+	if len(msg.Value) == 0 {
+		return ""
+	}
+	return msg.Value[0].Value
+}
+
+func (msg *ServerMessage) isError() bool {
+	if len(msg.Value) == 0 {
+		return false
+	}
+
+	return (strings.Split(msg.Value[0].Value, " ")[0] == "error")
 }
